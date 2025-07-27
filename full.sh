fa438ed65497d3a243027cf1893216bbf9683836
cyberman559 <cyberman559@gmail.com>
2025-07-27 04:24:38 +0300

    full and nocore

---
+++ full.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+source ./setting.conf
+
+export BORG_RSH="ssh -i ~/.ssh/id_ed25519_borg -p $SSH_PORT"
+export BORG_REMOTE_PATH="borg"
+
+FLAG_FILE="$HOME/.full_last_success"
+ARCHIVE_PREFIX="backup-full"
+
+function server_is_up() {
+    ssh -i ~/.ssh/id_ed25519_borg -o ConnectTimeout=5 -p "$SSH_PORT" "$SSH_USER@$SERVER_IP" "exit" &>/dev/null
+}
+
+function time_to_backup() {
+    if [[ ! -f "$FLAG_FILE" ]]; then
+        return 0
+    fi
+
+    last_date=$(<"$FLAG_FILE")
+    last_ts=$(date -d "$last_date" +%s)
+    next_ts=$(date -d "$last_date +7 days" +%s)
+    today_ts=$(date -d "$(date +%F)" +%s)
+
+    if [ "$today_ts" -ge "$next_ts" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function record_success() {
+    date +%F > "$FLAG_FILE"
+}
+
+function is_first_day_of_month() {
+    [ "$(date +%d)" = "01" ]
+}
+
+function create_backup() {
+    ARCHIVE_NAME="${ARCHIVE_PREFIX}-$(date +%Y-%m-%d_%H-%M-%S)"
+    borg create --verbose --stats --compression=lz4 \
+        --exclude-from full_excludes \
+        "$BORG_REPO"::"$ARCHIVE_NAME" "$BACKUP_SRC"
+}
+
+function prune_archives() {
+    borg prune -a "${ARCHIVE_PREFIX}-*" \
+        --keep-monthly=1 \
+        --keep-within=1d \
+        --verbose "$BORG_REPO"
+}
+
+function create_db_dump() {
+    mkdir -p "$DUMP_DIR"
+    mysqldump "$DB_NAME" > "$DUMP_FILE"
+    if [[ $? -ne 0 ]]; then
+        echo "Ошибка создания дампа базы данных."
+        return 1
+    fi
+    echo "Дамп базы данных сохранён в $DUMP_FILE"
+    return 0
+}
+
+function cleanup_db_dump() {
+    rm -f "$DUMP_FILE"
+}
+
+# --- Основная логика ---
+
+if server_is_up; then
+    if time_to_backup || is_first_day_of_month; then
+        echo "Сервер доступен, создаём резервную копию..."
+        create_db_dump;
+        if create_backup; then
+            echo "Резервная копия успешно создана."
+            record_success
+            prune_archives
+            cleanup_db_dump
+        else
+            echo "Ошибка создания резервной копии."
+        fi
+    else
+        echo "Резервная копия была менее 7 дней назад. Пропускаем."
+    fi
+else
+    echo "Сервер недоступен. Ничего не делаем."
+fi
--- nocore.sh
+++ nocore.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-source ../setting.conf
+source ./setting.conf
 
-# SSH + WSL + borg
 export BORG_RSH="ssh -i ~/.ssh/id_ed25519_borg -p $SSH_PORT"
 export BORG_REMOTE_PATH="borg"
 
-# Источник для бэкапа
-BACKUP_SRC="/home/bitrix/www"
-EXCLUDE_PATH="bitrix"
-
-# Контроль интервала между бэкапами
-FLAG_FILE="$HOME/.nocore_nobase_last_success"
-MAX_INTERVAL=$((24*3600))  # 24 часа
-
-# Название архивов
-ARCHIVE_PREFIX="backup-nocore-nobase"
+FLAG_FILE="$HOME/.full_last_success"
+ARCHIVE_PREFIX="backup-nocore"
 
 function server_is_up() {
     ssh -i ~/.ssh/id_ed25519_borg -o ConnectTimeout=5 -p "$SSH_PORT" "$SSH_USER@$SERVER_IP" "exit" &>/dev/null
@@ -25,24 +16,49 @@ function time_to_backup() {
     if [[ ! -f "$FLAG_FILE" ]]; then
         return 0
     fi
-    last_ts=$(<"$FLAG_FILE")
-    now_ts=$(date +%s)
-    (( now_ts - last_ts >= MAX_INTERVAL ))
+
+    last_date=$(<"$FLAG_FILE")
+    last_ts=$(date -d "$last_date" +%s)
+    next_ts=$(date -d "$last_date +1 days" +%s)
+    today_ts=$(date -d "$(date +%F)" +%s)
+
+    if [ "$today_ts" -ge "$next_ts" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function record_success() {
-    date +%s > "$FLAG_FILE"
+    date +%F > "$FLAG_FILE"
 }
 
 function create_backup() {
     ARCHIVE_NAME="${ARCHIVE_PREFIX}-$(date +%Y-%m-%d_%H-%M-%S)"
     borg create --verbose --stats --compression=lz4 \
-        --exclude "$BACKUP_SRC/$EXCLUDE_PATH" \
+        --exclude-from nocore_excludes \
         "$BORG_REPO"::"$ARCHIVE_NAME" "$BACKUP_SRC"
 }
 
 function prune_archives() {
-    borg prune -a "${ARCHIVE_PREFIX}-*" --keep-daily=3 --verbose "$BORG_REPO"
+    borg prune -a "${ARCHIVE_PREFIX}-*" \
+        --keep-within=3d \
+        --verbose "$BORG_REPO"
+}
+
+function create_db_dump() {
+    mkdir -p "$DUMP_DIR"
+    mysqldump "$DB_NAME" > "$DUMP_FILE"
+    if [[ $? -ne 0 ]]; then
+        echo "Ошибка создания дампа базы данных."
+        return 1
+    fi
+    echo "Дамп базы данных сохранён в $DUMP_FILE"
+    return 0
+}
+
+function cleanup_db_dump() {
+    rm -f "$DUMP_FILE"
 }
 
 # --- Основная логика ---
@@ -50,16 +66,17 @@ function prune_archives() {
 if server_is_up; then
     if time_to_backup; then
         echo "Сервер доступен, создаём резервную копию..."
-        create_backup
-        if [[ $? -eq 0 ]]; then
+        create_db_dump
+        if create_backup; then
             echo "Резервная копия успешно создана."
             record_success
             prune_archives
+            cleanup_db_dump
         else
             echo "Ошибка создания резервной копии."
         fi
     else
-        echo "Резервная копия делалась менее суток назад. Пропускаем."
+        echo "Резервная копия была менее 1 дня назад. Пропускаем."
     fi
 else
     echo "Сервер недоступен. Ничего не делаем."
