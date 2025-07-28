8ba5787e8fa1ee0a5ea7a8c9e2c87ec5d3eef0a1
cyberman559 <cyberman559@gmail.com>
2025-07-29 01:22:22 +0300

    SSH_USER

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ source ./setting.conf
 
 dnf install borgbackup -y
 
-ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519_borg -C "borg"
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519_borg -C "borg_$SSH_USER"
 echo "Публичный ключ:"
 cat ~/.ssh/id_ed25519_borg.pub
 read -p "Нажмите Enter, чтобы продолжить..."
