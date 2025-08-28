0fad4fa5bc7fed897088dffa1b205b7f544f615e
cyberman559 <cyberman559@gmail.com>
2025-08-28 23:40:44 +0300

    допил

--- start.sh
+++ start.sh
@@ -38,6 +38,48 @@ export BORG_PASSPHRASE=${BORG_PASSPHRASE}
 if time_to_backup; then
     date +%F > "$FLAG_RUN"
     trap 'rm -f "$FLAG_RUN"' EXIT
+
+    ssh -p "$PORT" -i /root/.ssh/id_ed25519 "$USER@$IP" \
+    DB_NAME="${DB_NAME[*]}" bash -s <<'EOF'
+        dump_base_skip_stat=1
+        dump_base_skip_search=1
+        dump_base_skip_log=1
+
+        IFS=' ' read -r -a DB_NAME <<< "$DB_NAME"
+
+        for db in "${DB_NAME[@]}"; do
+            all_tables=($(mysql -N -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA='$db';"))
+
+            IGNORE_ARGS=()
+
+            for table in "${all_tables[@]}"; do
+                table_lower=$(echo "$table" | tr '[:upper:]' '[:lower:]')
+
+                if [[ $dump_base_skip_stat -eq 1 && "$table_lower" =~ ^b_stat ]]; then
+                    IGNORE_ARGS+=(--ignore-table="$db.$table")
+                    continue
+                fi
+
+                if [[ $dump_base_skip_search -eq 1 && "$table_lower" =~ ^b_search_ ]]; then
+                    if [[ ! "$table_lower" =~ ^b_search_custom_rank$ && ! "$table_lower" =~ ^b_search_phrase$ ]]; then
+                        IGNORE_ARGS+=(--ignore-table="$db.$table")
+                        continue
+                    fi
+                fi
+
+                if [[ $dump_base_skip_log -eq 1 && "$table_lower" == "b_event_log" ]]; then
+                    IGNORE_ARGS+=(--ignore-table="$db.$table")
+                    continue
+                fi
+            done
+            mkdir -p /home/bitrix/db_dumps
+            mysqldump "${IGNORE_ARGS[@]}" "$db" > "/home/bitrix/db_dumps/$db.sql"
+            if [[ $? -ne 0 ]]; then
+                echo "Ошибка создания дампа базы данных."
+            fi
+        done
+EOF
+
     mkdir -p /mnt/${project}
     sshfs -p "$PORT" -o IdentityFile=/root/.ssh/id_ed25519 "$USER@$IP":/home/bitrix /mnt/${project}
 
